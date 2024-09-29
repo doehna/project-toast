782a0ff8ffe629f4aa8da6910b7acb3800bc3102
doehna <dorota.wojdecka@gmail.com>
2024-09-29 20:30:50 +0200

    Exercise 5

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
 
 const ICONS_BY_VARIANT = {
   notice: Info,
@@ -18,8 +18,8 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast({variant, handleXButtonClick, id, children }) {
-const Icon = ICONS_BY_VARIANT[variant];
+function Toast({ variant, handleXButtonClick, id, children }) {
+  const Icon = ICONS_BY_VARIANT[variant];
 
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
@@ -27,11 +27,16 @@ const Icon = ICONS_BY_VARIANT[variant];
         <Icon size={24} />
       </div>
       <p className={styles.content}>
+        <VisuallyHidden>{`${variant} - `}</VisuallyHidden>
         {children}
       </p>
-      <button className={styles.closeButton} onClick={() => handleXButtonClick(id)}>
+      <button
+        className={styles.closeButton}
+        onClick={() => handleXButtonClick(id)}
+        aria-label="Dismiss message"
+        aria-live="off"
+      >
         <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
     </div>
   );
